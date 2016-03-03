88830d0ec33f23e00e3d607b61368a6cde86995d
artur.vechorko <artur.vechorko@mainsoft.by>
2016-03-03 18:06:04 +0300

    rest

--- client/configuration/app.js
+++ client/configuration/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('codewars', ['ui.router', 'ui.bootstrap', 'btford.socket-io', 'restangular', 'ngAnimate']);
+var app = angular.module('codewars', ['ui.router', 'ui.bootstrap', 'btford.socket-io', 'ngAnimate']);
 
 app.run(['checkAccessFactory','$rootScope', function(checkAccessFactory, $rootScope) {
     $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState) {
