78d353becc291c9fbd271b753de066bbf9e0340d
Artur Vechorko <school26iko@gmail.com>
2016-03-16 00:01:15 +0300

    tasks server logic

--- client/content/states/tasksFilter/tasksFilterService.js
+++ client/content/states/tasksFilter/tasksFilterService.js
@@ -4,6 +4,11 @@ angular.module('codewars').factory('tasksFilterService', function($http , $q) {
             return $q(function(resolve, reject) {
                 $http.get('/types')
                     .success(function(types) {
+                        if(types) {
+                            types = types.map(function(type){
+                                return type.name;
+                            })
+                        }
                         resolve(types);
                     })
                     .error(function() {
--- server/application/router/routes/tasks.js
+++ server/application/router/routes/tasks.js
@@ -3,30 +3,65 @@ var _ = require('lodash');
 module.exports = function (req, res, next) {
     console.log(req.body);
     var group = {};
-    var where = "";
-    var join = "";
+    var where = [];
+    var join = [];
+    var sort = "";
+    var sortBy = "";
+    var filter = req.body.filter;
     if(req.session.authorized && req.body.filter) {
-        switch(req.body.filter.group){
+        switch(filter.group){
             case 'own':
-                where += ' where tasks.user_id = "' + req.session.userId + '"';
+                where.push( ' tasks.user_id = "' + req.session.userId + '" ' );
                 break;
             case 'favorite':
-                where += ' where favorites_tasks.user_id = "' + req.session.userId + '"';
-                join += ' join favorites_tasks using(task_id) ';
+                where.push( ' favorites_tasks.user_id = "' + req.session.userId + '" ');
+                join.push( ' join favorites_tasks using(task_id) ' );
                 break;
             case 'later':
-                where += ' where later_tasks.user_id = "' + req.session.userId + '"';
-                join += ' join later_tasks using(task_id) ';
+                where.push(' later_tasks.user_id = "' + req.session.userId + '" ');
+                join.push(' join later_tasks using(task_id) ');
                 break;
             default:
                 break;
         }
+        if(!_.isEmpty(filter.levels)){
+            where.push(' level in (' + filter.levels.join(',') + ') ' );
+        }
+        if(filter.sortOrder) {
+            sort = (filter.sortOrder == 'Z-A') ? 'DESC' : 'ASC' ;
+        }
+
+        console.log(filter.sortBy);
+        if(filter.sortBy){
+            if(filter.sortBy == 'date'){
+                sortBy = " order by add_date ";
+            }
+            if(filter.sortBy == 'name' || filter.sortBy == 'level'){
+                sortBy = " order by " + filter.sortBy + ' ';
+            }
+        }
+
+        if(!_.isEmpty(filter.types)) {
+            join.push(' join tasks_types using (task_id) join types using(type_id) ');
+            filter.types = '"' + filter.types.join('","') + '"';
+            where.push(' types.type_name in (' + filter.types + ') ');
+        }
+
+        if(_.isEmpty(where)){
+            where = "";
+        }else{
+            where.unshift('where ');
+            where = where.join(' ');
+        }
+        if(_.isEmpty(join)){
+            join = "";
+        }else{
+            join.join(' ');
+        }
     }
-    console.log(req.body.filter.group);
     var query = 'SELECT tasks.task_id as taskId, tasks.name as name, level, description, language, add_date as addDate' +
         ' from tasks' + (join || ' ') +
-        (where || ' ') +
-        ' ORDER BY addDate DESC' +
+        (where || ' ') + (sortBy || ' ') + (sort || ' ') +
         ' LIMIT 21 OFFSET ' + req.body.fromItem + ' ';
     console.log(query);
     connection.query(query, function(err,data){
