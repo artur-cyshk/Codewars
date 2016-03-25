b4fd1ba2b865b33d06dee9ee63382ffa4418327c
Artur Vechorko <school26iko@gmail.com>
2016-03-26 01:37:31 +0300

    leaders with avatar

--- server/application/router/routes/leaders.js
+++ server/application/router/routes/leaders.js
@@ -1,6 +1,7 @@
 var connection = require('../../../configuration/database/connection');
+var _ = require('lodash');
 module.exports = function (req, res, next) {
-    var query = 'select name, clan, honor from users' +
+    var query = 'select name, clan, honor,avatar_url as avatarUrl from users' +
         ' order by honor DESC, name limit 21 OFFSET ' + req.params.page ;
     connection.query(query, function(err, data){
         if(err || !data) {
