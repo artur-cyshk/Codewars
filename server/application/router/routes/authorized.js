020e890d601b3c8e0a33cbd4b801d0c31019a941
Artur Vechorko <school26iko@gmail.com>
2016-04-08 22:44:02 +0300

    bug fix

--- server/application/router/routes/authorized.js
+++ server/application/router/routes/authorized.js
@@ -1,4 +1,5 @@
 var connection = require('../../../configuration/database/connection');
+var _ = require('lodash');
 module.exports = function (req, res, next) {
 	if(!req.session.authorized) {
 		return next({
@@ -8,7 +9,7 @@ module.exports = function (req, res, next) {
 		var query = 'select user_id as userId, type from users where user_id=?';
 		connection.query(query, req.session.userId,
 			function(err, data) {
-				if(err || !data){
+				if(err || !_.isObject(data) || !_.isObject(data[0])){
 					return next(true);
 				}
 				req.session.currUserRole = data[0].type;
